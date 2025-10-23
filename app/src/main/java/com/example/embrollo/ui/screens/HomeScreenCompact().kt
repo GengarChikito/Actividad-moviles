07c5279069a141d00cf436fcf43ecfc0ce4beb79
GengarChikito <math.cortes@duocuc.cl>
2025-10-22 23:02:42 -0300

    ayuda

---
+++ app/src/main/java/com/example/embrollo/ui/screens/HomeScreenCompact().kt
@@ -0,0 +1,71 @@
+package com.example.embrollo.ui.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.runtime.Composable
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.material3.Text
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.embrollo.R
+import com.example.embrollo.ui.theme.EmbrolloTheme
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreenCompact() {
+    Scaffold (
+        topBar = {
+            TopAppBar(title = { Text(text = "Mi App Kotlin") })
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(20.dp)
+        ){
+            Text(
+                text = "¡Bienvenido",
+                color = MaterialTheme.colorScheme.primary,
+                style = MaterialTheme.typography.titleLarge
+            )
+
+            Button(onClick = { /* acción para dps */ }) {
+                Text("Presioname")
+            }
+
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "Logo App",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(150.dp),
+                contentScale = ContentScale.Fit
+            )
+            //Errores en rojo seguramente se arreglen con el codigo completo
+        }
+    }
+}
+
+@Preview(name = "Compact", widthDp = 360, heightDp = 800)
+@Composable
+fun PreviewCompact() {
+    EmbrolloTheme {
+        HomeScreenCompact()
+    }
+}
+
---
+++ app/src/main/java/com/example/embrollo/ui/utils/WindowSizeUtils.kt
@@ -0,0 +1,15 @@
+import android.app.Activity
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+
+@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
+@Composable
+fun obtenerWindowSizeClass(): WindowSizeClass {
+    val activity = (LocalContext.current as? Activity)
+        ?: throw IllegalStateException("Solo se puede usar en ejecución real")
+    return calculateWindowSizeClass(activity)
+}
+
